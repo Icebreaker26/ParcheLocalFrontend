9f0376221a5f1ae182311a1aacab20afcb3212f7
Icebreaker26 <alejandro.torres0826@gmail.com>
2026-05-03 22:16:32 -0500

    docs: Preparando frontend para deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    // Si existe una variable de entorno la usa, si no, usa localhost (para desarrollo)
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api'
 });
 
 export default api;
