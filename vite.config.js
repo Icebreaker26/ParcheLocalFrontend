08e13d16beaef482b1beffd5970cd3444d3dbf30
Icebreaker26 <alejandro.torres0826@gmail.com>
2026-05-03 22:41:21 -0500

    docs vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import react from '@vitejs/react-swc'
 
 export default defineConfig({
   plugins: [react()],
+
   preview: {
-    allowedHosts: 'all'
+    allowedHosts: true 
   },
   server: {
-    allowedHosts: 'all'
+    allowedHosts: true
   }
 })
